59c20a978971d91f433e8dd18d3c7930f0868213
sachour <sachour@mit.edu>
2015-07-08 10:04:06 -0400

    added derivative, equality support

--- src/SymCaml.ml
+++ src/SymCaml.ml
@@ -15,6 +15,7 @@ sig
    val define_symbol : symcaml ->  string -> symexpr
    val define_expr : symcaml -> string -> symexpr -> symexpr
    val define_wildcard: symcaml -> string -> symexpr list -> symexpr
+   val define_function: symcaml -> string -> symexpr
    val clear : symcaml -> unit
    val expr2py : symcaml -> symexpr -> string
 
@@ -22,6 +23,7 @@ sig
    val eval : symcaml -> symexpr -> symexpr
    val simpl : symcaml -> symexpr -> symexpr
    val pattern: symcaml -> symexpr -> symexpr -> (string*symexpr) list
+   val report : symcaml -> unit
 end = 
 struct 
    type symcaml = {
@@ -61,16 +63,32 @@ struct
          | Paren(e) -> "("^(_expr2py e)^")" 
          | Decimal(x) -> string_of_float x
          | Integer(x) -> string_of_int x
+         | Div(n,d) -> (_expr2py (Paren n))^"/"^(_expr2py (Paren d))
+         | Function(x,lst) -> x^"("^(exprlst2py (fun x r -> r^","^(_expr2py x)) lst)^")"
+         | Deriv(e,lst) -> "Derivative("^(_expr2py e)^
+            (List.fold_right (
+               fun (v,n) r -> 
+                  let (sv,_) = PyCamlWrapper.get_var (_wr s) v in
+                  let sn = string_of_int n in
+                  r^","^sv^","^sn
+            ) lst "")^")"
+         | Eq(lhs,rhs) -> "Eq("^(_expr2py lhs)^","^(_expr2py rhs)^")"
       in 
          _expr2py e
 
    let define_symbol (s:symcaml) (x:string) : symexpr =
-      let _ = PyCamlWrapper.define (_wr s) x ("Symbol(\""^x^"\")") in 
+      let cmd = "Symbol(\""^x^"\")" in
+      let _ = PyCamlWrapper.define (_wr s) x cmd in 
       (Symbol x)
 
+   let define_function (s:symcaml) (x:string) : symexpr =
+      let cmd = "Function(\""^x^"\")" in
+      let _ = PyCamlWrapper.define (_wr s) x cmd in 
+      (Symbol x)
 
    let define_expr (s:symcaml) (x:string) (e:symexpr) = 
-      let _ = PyCamlWrapper.define (_wr s) x (expr2py s e) in 
+      let stre = expr2py s e in
+      let _ = PyCamlWrapper.define (_wr s) x stre in 
       Symbol(x)
 
    let define_wildcard (s:symcaml) (x:string) (exns:symexpr list) : symexpr = 
@@ -78,7 +96,8 @@ struct
          | h::t -> "["^(List.fold_right (fun x r -> r^","^(expr2py s x)) t (expr2py s h))^"]"
          | [] -> "[]"
       in 
-      let _ = PyCamlWrapper.define (_wr s) x (" Wild(\""^x^"\",exclude="^opt_arg^")") in 
+      let cmd ="Wild(\""^x^"\",exclude="^opt_arg^")" in 
+      let _ = PyCamlWrapper.define (_wr s) x cmd in 
       Symbol(x)
 
    let _pyobj2expr (s:symcaml) (p:pyobject) : symexpr = 
@@ -161,15 +180,7 @@ struct
             end
          | _ -> raise (SymCamlFunctionException("pattern","unexpected: null callee or argument."))
       
-      (*
-      let ntmp = "__tmp__" in
-      let tmp = _env ntmp in 
-      let arg = _rslv_expr (fun x -> "("^x^")") targ in 
-      let cmd = _rslv_expr (fun x -> tmp^"="^x^".match"^arg) e in
-      Printf.printf "CMD: %s\n" cmd;
-      let _ = pyrun_simplestring(cmd) in
-      _toexprstruct s ntmp;
-      []
-      *)
+   let report (s:symcaml) : unit =
+      PyCamlWrapper.report (_wr s)
 
 end
